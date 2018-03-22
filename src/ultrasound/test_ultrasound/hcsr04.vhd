0aa6f757122c569710f6552052908caef6e064a4
orphee.antoniad <orphee.antoniadis@etu.hesge.ch>
2018-03-22 17:48:47 +0100

    modify a little vhdl code for ultrasound. The problem is that I use a 16 bits register and it is not enough

--- src/ultrasound/test_ultrasound/hcsr04.vhd
+++ src/ultrasound/test_ultrasound/hcsr04.vhd
@@ -13,10 +13,13 @@ entity hcsr04 is
 end hcsr04;
 
 architecture behavior of hcsr04 is
-	constant rate_100ms : std_logic_vector(8 downto 0) := std_logic_vector(to_unsigned(500, 9));
-	signal en_count : std_logic;
+	constant rate_10us : std_logic_vector(8 downto 0) := std_logic_vector(to_unsigned(500, 9));
+	signal en_trig : std_logic;
 	signal trig_count : std_logic_vector(8 downto 0);
 	signal echo_count : unsigned(15 downto 0);
+	signal rst_timeout : std_logic;
+	signal en_timeout : std_logic;
+	signal timeout : std_logic_vector(8 downto 0);
 	
 	component counter
 		generic(size: integer := 9);
@@ -37,10 +40,21 @@ begin
 		clk => clk,
 		rst => rst,
 		load => '0',
-		enable => en_count,
+		enable => en_trig,
 		load_value => (others => '0'),
-		max_value => rate_100ms,
+		max_value => rate_10us,
 		counter_value => trig_count
+	);
+	
+	CNT2 : counter generic map (size => 9)
+	port map (
+		clk => clk,
+		rst => rst_timeout,
+		load => '0',
+		enable => en_timeout,
+		load_value => (others => '0'),
+		max_value => rate_10us,
+		counter_value => timeout
 	);
 	
 	process(clk)
@@ -51,30 +65,40 @@ begin
 	begin
 		if rst = '1' then
 			trig <= '0';
-			en_count <= '0';
+			en_trig <= '0';
 			echo_count <= (others => '0');
+			rst_timeout <= '1';
+			en_timeout <= '0';
 			computing := '0';
 			init := '0';
 			calc_dist := (others => '0');
 		elsif rising_edge(clk) then
 			if computing = '0' then
 				trig <= '1';
-				en_count <= '1';
+				en_trig <= '1';
 				echo_count <= (others => '0');
+				rst_timeout <= '0';
 				computing := '1';
-				init := '1';
 			else
-				if trig_count = rate_100ms then
+				if trig_count = rate_10us then
 					trig <= '0';
-					en_count <= '0';
+					en_trig <= '0';
 					init := '1';
+					en_timeout <= '1';
 				elsif init = '1' then
 						if echo = '1' then
 							echo_count <= echo_count + 1;
 						elsif std_logic_vector(echo_count) /= "0000000000000000" then
+							en_timeout <= '0';
+							rst_timeout <= '1';
 							computing := '0';
 							init := '0';
 							calc_dist := std_logic_vector(echo_count);
+						elsif std_logic_vector(timeout) = rate_10us then
+							en_timeout <= '0';
+							rst_timeout <= '1';
+							computing := '0';
+							init := '0';
 						end if;
 					end if;
 			end if;
