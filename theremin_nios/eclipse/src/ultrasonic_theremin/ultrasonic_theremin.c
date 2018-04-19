2a808e8eb944a5c86609e9d9007984e071210080
orphee.antoniad <orphee.antoniadis@etu.hesge.ch>
2018-04-19 10:39:44 +0200

    ultrasound distance level display on matrix done

--- theremin_nios/eclipse/src/ultrasonic_theremin/ultrasonic_theremin.c
+++ theremin_nios/eclipse/src/ultrasonic_theremin/ultrasonic_theremin.c
@@ -14,12 +14,63 @@
 #include "os/alt_hooks.h"
 #include "alt_types.h"
 
+/*-------------------------------------------------------*/
+/*									Switch led functions								 */
+/*-------------------------------------------------------*/
+
+static inline void init_switch_leds(void) {
+	IOWR_8DIRECT(LEDSA_BASE, 0, 0xff);
+	IOWR_8DIRECT(LEDSB_BASE, 0, 0xff);
+}
+
+static inline void set_switch_ledA(uint8_t data) { IOWR_8DIRECT(LEDSA_BASE, 0, data); }
+static inline void set_switch_ledB(uint8_t data) { IOWR_8DIRECT(LEDSB_BASE, 0, data); }
+
+/*-------------------------------------------------------*/
+/*									Ultrasound functions								 */
+/*-------------------------------------------------------*/
+
+int get_distance(uint16_t ultrasound) {
+	uint32_t ticks;
+	uint8_t ticks_LSB, ticks_MSB;
+	float time, dist;
+
+	ticks = IORD_32DIRECT(ultrasound, 0);
+	time = (ticks * 20) * 1e-3;
+	dist = time / 58;
+
+	ticks_LSB = (ticks >> 8) & 0xff;
+	ticks_MSB = (ticks >> 16) & 0xff;
+
+	set_switch_ledA(ticks_MSB);
+	set_switch_ledB(ticks_LSB);
+
+	return (int)dist;
+}
+
+int get_amplitude(int distance) {
+	// hand very close to the ultrasound so max amplitude
+	if (distance < 3) {
+		return 8;
+	// hand far from the ultrasound so min amplitude
+	} else if (distance > 35) {
+		return 0;
+	// amplitude level calculation (we got 8 levels of 4 points of amplitude)
+	} else {
+		return (32 - (distance - 3)) / 4);
+	}
+}
+
+/*-------------------------------------------------------*/
+/*									LED MATRIX functions								 */
+/*-------------------------------------------------------*/
+
 #define PIXEL_COUNT		96
 #define MATRIX_RED		4
 #define MATRIX_GREEN	2
 #define MATRIX_BLUE		1
 
-const uint8_t empty_matrix[96] =
+const uint8_t empty_matrix[PIXEL_COUNT] =
 {
 		0,0,0,0,0,0,0,0,0,0,0,0,
 		0,0,0,0,0,0,0,0,0,0,0,0,
@@ -31,43 +82,13 @@ const uint8_t empty_matrix[96] =
 		0,0,0,0,0,0,0,0,0,0,0,0,
 };
 
-static inline void init_switch_leds(void) {
-	IOWR_8DIRECT(LEDSA_BASE, 0, 0xff);
-	IOWR_8DIRECT(LEDSB_BASE, 0, 0xff);
-}
-
-static inline void set_switch_ledA(uint8_t data) { IOWR_8DIRECT(LEDSA_BASE, 0, data); }
-static inline void set_switch_ledB(uint8_t data) { IOWR_8DIRECT(LEDSB_BASE, 0, data); }
-
-float get_distance0(void) {
-	uint32_t ticks;
-	uint8_t ticks_LSB, ticks_MSB;
-	float time, dist;
-
-	ticks = IORD_32DIRECT(ULTRASOUND_0_BASE, 0);
-	time = (ticks * 20) * 1e-3;
-	dist = time / 58;
-
-	ticks_LSB = (ticks >> 8) & 0xff;
-	ticks_MSB = (ticks >> 16) & 0xff;
-
-	set_switch_ledA(ticks_MSB);
-	set_switch_ledB(ticks_LSB);
-
-	return dist;
-}
-
-/*-------------------------------------------------------*/
-/*									LED MATRIX functions								 */
-/*-------------------------------------------------------*/
-
 static inline void set_pixel(uint8_t x, uint8_t y, uint8_t rgb) { IOWR_16DIRECT(LEDMATRIX_0_BASE, (y*12+x)*2, rgb); }
 
 void set_column(uint8_t x, uint8_t amplitude, uint8_t rgb) {
-	uint8_t i;
+	int i;
 	
 	for (i = 7; i > (7-amplitude); i--) {
-		set_pixel(x, i, MATRIX_GREEN);
+		set_pixel(x, i, rgb);
 	}
 }
 
@@ -79,6 +100,10 @@ void set_matrix(uint8_t matrix[PIXEL_COUNT]) {
 	}
 }
 
+/*-------------------------------------------------------*/
+/*												Main													 */
+/*-------------------------------------------------------*/
+
 int main() {
 	float distance;
 
@@ -86,6 +111,8 @@ int main() {
 	set_matrix(empty_matrix);
 
 	while(1) {
-		distance = get_distance0();
+		// display first ultrasound measure
+		distance = get_distance(ULTRASOUND_0_BASE);
+		set_column(2, get_amplitude(distance), MATRIX_GREEN);
 	}
 }
